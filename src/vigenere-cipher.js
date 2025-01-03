e3f9ebdb1c8be918098fbc09bc51427e0ed873cf
TRAHIM86 <trahim1986@gmail.com>
2025-01-03 19:13:08 +0300

    feat: completed task 4

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,55 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(mode) { this.mode = mode } 
+
+  encrypt(text, code) {
+    if (!text || !code) {throw new Error("Incorrect arguments!")};
+    let textArr = text.toUpperCase().split('');
+    let codeArr = code.toUpperCase().split('');
+    //console.log(textArr, codeArr)
+    let j = 0;
+    for (let i = 0; i < textArr.length; i++) {
+      
+      
+      if (textArr[i].charCodeAt() < 65 || textArr[i].charCodeAt() > 90) {
+            textArr[i] = textArr[i];
+      } else {
+        if ((textArr[i].charCodeAt() + codeArr[j].charCodeAt()) > 155) {
+          textArr[i] = textArr[i].charCodeAt() + codeArr[j].charCodeAt() - 91;
+          textArr[i] = String.fromCharCode(textArr[i]);
+        } else {
+          textArr[i] = textArr[i].charCodeAt() + codeArr[j].charCodeAt() - 65;
+          textArr[i] = String.fromCharCode(textArr[i]);
+        } j++
+      }
+      if (j === codeArr.length) {j = 0}
+    }
+    return this.mode === false ? textArr.reverse().join('') : textArr.join('');
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  decrypt(text, code) {
+    if (!text || !code) {throw new Error("Incorrect arguments!")};
+    let textArr = text.toUpperCase().split('');
+    let codeArr = code.toUpperCase().split('');
+    let j = 0;
+    for (let i = 0; i < textArr.length; i++) {
+      if (textArr[i].charCodeAt() < 65 || textArr[i].charCodeAt() > 90) {
+        textArr[i] = textArr[i];
+      } else {
+        if ((textArr[i].charCodeAt() - codeArr[j].charCodeAt()) >= 0) {
+          textArr[i] = textArr[i].charCodeAt() - codeArr[j].charCodeAt() + 65;
+          textArr[i] = String.fromCharCode(textArr[i]);
+        } else {
+          textArr[i] = textArr[i].charCodeAt() - codeArr[j].charCodeAt() + 91;
+          textArr[i] = String.fromCharCode(textArr[i]);
+        } 
+        j++;
+      }
+      if (j === codeArr.length) {j = 0}
+    }
+    return this.mode === false ? textArr.reverse().join('') : textArr.join(''); 
   }
 }
 
